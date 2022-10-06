90f2cc80b8932e9029b3f4f1357f39668ce216fb
KoahF <khoakg2013@gmail.com>
2022-10-06 15:24:23 +0700

    Display random movie in Banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { baseUrl } from '../constants/movies';
+import { Movie } from '../typings';
 
-const Banner = () => {
-	return (
-		<div>Banner</div>
-	)
+interface Props {
+	netflixOriginals: Movie[];
 }
 
-export default Banner
+const Banner = ({ netflixOriginals }: Props) => {
+	const [movie, setMovie] = useState<Movie | null>(null);
+
+	useEffect(() => {
+		setMovie(
+			netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+		);
+	}, [netflixOriginals]);
+
+	console.log(movie);
+	return (
+		<div>
+			<div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+				<Image
+					src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,6 @@ const Home: FC<Props> = ({
 	topRated,
 	trendingNow,
 }) => {
-	console.log(netflixOriginals);
 	return (
 		<div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
 			<Head>
@@ -39,7 +38,7 @@ const Home: FC<Props> = ({
 
 			<main>
 				{/* Banner */}
-				<Banner />
+				<Banner netflixOriginals={netflixOriginals} />
 				<section>
 					{/* Row */}
 					{/* Row */}
